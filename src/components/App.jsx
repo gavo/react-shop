001d5d9198b4f4e76780a080b1ced9743b9b80e5
Gustavo Vargas Miranda <gvmgust@gmail.com>
2022-10-23 17:17:03 -0400

    Configurando Estilos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import "../styles/global.scss";
+import Layout from "../containers/Layout";
+import "../styles/global.css";
 
 const App = () => {
-  return <h1>Hola Mundo</h1>;
+  return (
+    <Layout>
+      <Login />
+    </Layout>
+  );
 };
 
 export default App;
